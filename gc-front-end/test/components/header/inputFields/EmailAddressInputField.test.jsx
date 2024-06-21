afc74fa001e833bdca21baa600c83a6b04d75939
Ben Turner <turner.benjamin88@gmail.com>
2024-06-21 08:41:39 +0100

    PASSED: US2-EAI-2
    It should display an error on blur where email address is invalid

--- gc-front-end/test/components/header/inputFields/EmailAddressInputField.test.jsx
+++ gc-front-end/test/components/header/inputFields/EmailAddressInputField.test.jsx
@@ -5,8 +5,27 @@ describe("Email Address input field tests", () => {
   //?US2-EAI-1
   test("It should not display an error on render", () => {
     render(
-      <EmailAddressInputField emailAddressValue="" onChange={() => null} />
+      <EmailAddressInputField
+        emailAddressValue="invalid.email"
+        onChange={() => null}
+      />
     );
     expect(screen.queryByRole("alert")).toBe(null);
+  });
+
+  //?US2-EAI-2
+  test("It should display an error on blur where email address is invalid", async () => {
+    render(
+      <EmailAddressInputField
+        emailAddressValue="invalid.email"
+        onChange={() => null}
+      />
+    );
+    const inputField = screen.getByTitle("Email address");
+    await act(async () => {
+      inputField.focus();
+      inputField.blur();
+    });
+    expect(screen.queryByRole("alert")).toBeInTheDocument();
   });
 });
