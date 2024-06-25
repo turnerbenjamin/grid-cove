b387a097cb30dc54a39fb43efecafd58364a2eb1
Ben Turner <turner.benjamin88@gmail.com>
2024-06-25 09:31:30 +0100

    PASSED: US6-PVR-3
    It should return false and an errors array with a length greater than 0 where title is more than 32 characters

--- gc-front-end/test/utils/PuzzleValidator.test.js
+++ gc-front-end/test/utils/PuzzleValidator.test.js
@@ -36,4 +36,16 @@ describe("Register tests", () => {
     expect(actualIsValidated).toBe(false);
     expect(actualErrors.length).greaterThan(0);
   });
+
+  //?US6-PVR-3
+  test("It should return false and an errors array with a length greater than 0 where title is more than 32 characters", () => {
+    //Arrange
+    testSubmission.title = "a".repeat(33);
+    //Act
+    const [actualIsValidated, actualErrors] =
+      PuzzleValidator.validate(testSubmission);
+    //Assert
+    expect(actualIsValidated).toBe(false);
+    expect(actualErrors.length).greaterThan(0);
+  });
 });
