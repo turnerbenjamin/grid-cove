9111dcfdf5bc91c38e8a45bcd27732bc721c931a
Ben Turner <turner.benjamin88@gmail.com>
2024-06-27 08:19:23 +0100

    PASSED: US9-GSS-2
    It should return true for a given row where it's state matches the solution

--- gc-front-end/test/utils/GridSolveState.test.js
+++ gc-front-end/test/utils/GridSolveState.test.js
@@ -5,27 +5,38 @@ import GridSolveState from "../../src/utils/GridSolveState";
 describe("Grid Solve State tests: ", () => {
   //Note, this test puzzle is purposely invalid, it has more than one potential solution.
   const testPuzzle = {
-    size: 3,
+    size: 5,
     clues: {
       rowClues: [[1, 1], [1, 1], [1], [1, 1], [1, 1]],
       columnClues: [[1, 1], [1, 1], [1], [1, 1], [1, 1]],
     },
     solution: "1000101010001000101010001",
   };
-  let emptyGridFillString;
+  let testGridFillString;
   beforeEach(() => {
-    emptyGridFillString = "0".repeat(25);
+    testGridFillString = "0".repeat(25);
   });
 
   afterEach(() => {
-    emptyGridFillString = null;
+    testGridFillString = null;
   });
 
   //?US9-GSS-1
   test("It should return false for a given row where the state of that row does not match its clue signature", () => {
     //Act
-    const gridSolveState = new GridSolveState(emptyGridFillString, testPuzzle);
+    const gridSolveState = new GridSolveState(testGridFillString, testPuzzle);
     //Assert
     expect(gridSolveState.getRowSolveState(0)).toEqual(false);
+  });
+
+  //?US9-GSS-2
+  test(" It should return true for a given row where it's state matches the solution", () => {
+    //Arrange
+    testGridFillString =
+      testPuzzle.solution.slice(0, 5) + testGridFillString.slice(5);
+    //Act
+    const gridSolveState = new GridSolveState(testGridFillString, testPuzzle);
+    //Assert
+    expect(gridSolveState.getRowSolveState(0)).toEqual(true);
   });
 });
